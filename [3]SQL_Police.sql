f35a157c85c9f908cdd95b133d94a7b883f2e379
zeraphimGCash <john.diamante@gcash.com>
2024-08-12 15:23:55 +0800

    SQL Police Department Cases: Added 5 more cases for a total of 15 cases solved

--- [3]SQL_Police.sql
+++ [3]SQL_Police.sql
@@ -55,3 +55,34 @@ SELECT *
 FROM mailing_ list
 ORDER BY LastName;
 
+-- 11. A mailing list of an illegal online service was sent to the SQLPD hot-line. Please submit all entries' details sorted by join dates in descending order.
+
+SELECT *
+FROM mailing_list
+ORDER BY Joined DESC;
+
+-- 12. An illegal site's servers were seized in a recent operation. Please submit all users first names, surnames and access times' details sorted by surnames in descending order. Please make sure there are no duplicates.
+
+SELECT DISTINCT FirstName, Surname, AccessTime
+FROM users
+ORDER BY Surname DESC;
+
+-- 13. White hat hacker has sent SQLPD exposed members' details of a shady site connected to various persons of interest. Please submit all members joined on dates and number of purchases' details sorted by joined on dates in descending order and then by number of purchases in ascending order.
+
+SELECT JoinedOn, Purchases
+FROM members
+ORDER BY JoinedOn DESC, Purchases;
+
+-- 14. White hat hacker has sent SQLPD exposed members' details of a shady site connected to various persons of interest. Please submit the top 20 members' details when sorted by number of comments in descending order and then by password hashes in descending order.
+
+SELECT *
+FROM members
+ORDER BY NumberOfComments DESC, PasswordHash DESC
+LIMIT 20;
+
+-- 15. A hacked site subscribers' details have surfaced on a darknet forum. Please submit the top 20 subscribers addresses and password hashes' details when sorted by password hashes in ascending order and then by addresses in descending order. Please make sure there are no duplicates.
+
+SELECT DISTINCT Address, PasswordHash
+FROM subscribers
+ORDER BY PasswordHash, Address DESC
+LIMIT 20;
